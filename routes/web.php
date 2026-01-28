f9af447e9eb9a96fffca61f0a2ef4d83df3e6cb4
hendryriq <hendryriq@gmail.com>
2026-01-28 15:23:22 +0700

    add SubscriptionPlanController and route

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,11 @@ Route::middleware(['auth', 'role:user'])->prefix('dashboard')->name('user.dashbo
     
     Route::get('movie/{movie:slug}', [MovieController::class, 'show'])->name('movie.show')->middleware('checkUserSubscription:true');
 
-    // Route::get('subscription-plan', [SubscriptionPlanController::class, 'index'])->name('subscriptionPlan.index')->middleware('checkUserSubscription:false');
+    Route::get('subscription-plan', [SubscriptionPlanController::class, 'index'])->name('subscriptionPlan.index')->middleware('checkUserSubscription:false');
     // Route::post('subscription-plan/{subscriptionPlan}/user-subscribe', [SubscriptionPlanController::class, 'userSubscribe'])->name('subscriptionPlan.userSubscribe')->middleware('checkUserSubscription:false');
 });
 
-Route::redirect('/', '/prototype/login'); 
+Route::redirect('/', '/login'); 
 
 Route::get('/dashboard', function () {
     return Inertia::render('Prototype/Dashboard');
